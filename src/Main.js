302306d7c4ebb7335337715fa3711fd55a511579
lingyun1010 <zhaolingyun1010@gmail.com>
2019-03-28 10:48:13 +0000

    Restore the selectOpacity value

--- src/Main.js
+++ src/Main.js
@@ -63,7 +63,7 @@ const addColoringScheme  = compose(
     selectColour: `purple`,
     showOpacity: 0.4,
     highlightOpacity: 0.4,
-    //selectOpacity: 0.4
+    selectOpacity: 0.4
   }),
   withPropsOnChange(negate(isEqual),
     props => ({idsWithMarkup: idsWithMarkupAccordingToCurrentColoringScheme(props)})
