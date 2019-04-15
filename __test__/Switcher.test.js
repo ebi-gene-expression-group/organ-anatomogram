89ace8038d88a93d2dc79992b0e81e9d727fccb4
lingyun1010 <zhaolingyun1010@gmail.com>
2019-04-15 16:34:56 +0100

    Amend, add tests and update snapshots

--- __test__/Switcher.test.js
+++ __test__/Switcher.test.js
@@ -5,23 +5,35 @@ import { mount } from 'enzyme'
 import Switcher from '../src/Switcher'
 
 import svgsMetadata from '../src/json/svgsMetadata.json'
-import {getAnatomogramViews, getDefaultView} from '../src/Assets'
+import {unique, getAnatomogramViews, getDefaultView, getParentView} from '../src/Assets'
 
-const unique = (value, index, self) => self.indexOf(value) === index
 const allSpecies = svgsMetadata
   .map((svgMetadata) => svgMetadata.species)
   .filter(unique)
   .sort()
+
+const allChildrenViews = species =>
+  svgsMetadata.filter(svgsMetadata => svgsMetadata.species === species)
+    .filter(view => view.hasOwnProperty(`parent`))
+    .map(data => data.view)
 
 describe(`Anatomogram switcher`, () => {
   const requiredProps = {
     onChangeView: () => {}
   }
 
-  test(`should contain as many buttons as views are defined for a species`, function() {
-    allSpecies.forEach((species) => {
+  test(`should contain as many buttons as views are defined for a species apart from kidney`, function() {
+    allSpecies.filter(species => species !== `kidney`).forEach((species) => {
       expect(mount(<Switcher {...requiredProps} species={species}/>).find(`img`)).toHaveLength(getAnatomogramViews(species).length)
     })
+  })
+
+  test(`should contain a backward button for parent tissue parts for kidney species`, function() {
+    const species = `kidney`
+    allChildrenViews(species).forEach(view =>{
+      expect(mount(<Switcher {...requiredProps} species={species} parentView={getParentView(species, view)}/>).find(`img`)).toHaveLength(1)
+    }
+    )
   })
 
   test(`should respond to onClick events with the anatomogram view`, () => {
