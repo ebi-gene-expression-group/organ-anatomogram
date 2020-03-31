299eebb0f66479e185f78b0e4b83f1eff8202d1b
lingyun1010 <zhaolingyun1010@gmail.com>
2020-03-31 09:16:31 +0100

    Correct tests and update snapshots

--- __test__/Switcher.test.js
+++ __test__/Switcher.test.js
@@ -36,14 +36,14 @@ describe(`Anatomogram switcher`, () => {
     )
   })
 
-  test(`should respond to onClick events with the anatomogram view`, () => {
+  test(`should respond to onClick events with the anatomogram view and corresponding species`, () => {
     const onButtonClick = jest.fn()
     const wrapper = mount(<Switcher {...requiredProps} organs={[`kidney`]} species={`homo_sapiens`} onChangeView={onButtonClick}/>)
     wrapper.find(`img`).first().simulate(`click`)
     wrapper.find(`img`).last().simulate(`click`)
     expect(onButtonClick.mock.calls.length).toBe(2)
-    expect(onButtonClick.mock.calls[0][0]).toBe(getAnatomogramViews(`homo_sapiens`)[0])
-    expect(onButtonClick.mock.calls[1][0]).toBe(getAnatomogramViews(`homo_sapiens`)[2])
+    expect(onButtonClick.mock.calls[0]).toEqual([`homo_sapiens`, getAnatomogramViews(`homo_sapiens`)[0]])
+    expect(onButtonClick.mock.calls[1]).toEqual([`homo_sapiens`, getAnatomogramViews(`homo_sapiens`)[2]])
   })
 
   allSpecies.forEach((species) => {
